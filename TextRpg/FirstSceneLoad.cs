bebdf7bb3f74d2191d10aff670af3d53d629a6fc
zooo980801 <45661592+zooo980801@users.noreply.github.com>
2025-04-17 22:26:51 +0900

    Add files via upload

---
+++ TextRpg/FirstSceneLoad.cs
@@ -0,0 +1,24 @@
+﻿using System.Buffers.Text;
+using System.Xml.Linq;
+using Amazon.S3.Model;
+using TextRpg;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace TextRpg
+{
+    internal class FirstSceneLoad
+{
+    static void Main(string[] args)
+    {
+        fistScene scene = new fistScene();
+        scene.First(); // 이름만 받음
+
+        Stats stats = new Stats();
+        stats.GenerateStats(); // 랜덤 스탯 생성
+
+        GameManagement gameManagement = new GameManagement(scene.PlayerName, stats);
+        Stage stage = new Stage(scene.PlayerName, stats, gameManagement);
+        stage.getSelect(); // 게임 시작
+    }
+}
+}
---
+++ TextRpg/Itemlist.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRpg
+{
+    public class Itemlist
+    {
+        public string ItemName { get; set; }
+        public string ToolTip { get; set; }
+        public int Attack { get; set; }  // 공격력
+        public int Defense { get; set; } // 방어력
+        public int Price { get; set; }   // 가격
+        public bool Purchase { get; set; } // 구매 여부
+        public int Quantity { get; set; } // 구매한 수량을 기록하는 변수
+        public bool IsEquipped { get; set; } // 장착 여부 추가
+
+        // 생성자
+        public Itemlist(string itemName, string toolTip, int attack, int defense, int price)
+        {
+            ItemName = itemName;
+            ToolTip = toolTip;
+            Attack = attack;
+            Defense = defense;
+            Price = price;
+            Purchase = false;
+            IsEquipped = false;
+            Quantity = 1;
+        }
+
+        public bool Equip()
+        {
+            if (Quantity > 0 && !IsEquipped) 
+            {
+                IsEquipped = true;
+                return true;
+            }
+            return false; 
+        }
+
+        public bool Unequip()
+        {
+            if (IsEquipped) 
+            {
+                IsEquipped = false;
+                return true;
+            }
+            return false; 
+        }
+
+    }
+}
